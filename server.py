c7688bff9e4387df19e78e91839cc192b479116d
David Yates <David@recursiveav.co.uk>
2023-01-04 17:20:10 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,24 +1,29 @@
 import os
 import http.server
 import socketserver
+import pathlib
 
 from http import HTTPStatus
-from http.server import HTTPServer, BaseHTTPRequestHandler
 
-class Server(BaseHTTPRequestHandler):
+
+class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-
-        # get the page
-        path = self.path
-        if path == '/':
-            with open('home.html', 'rb') as page:
-                self.wfile.write(page.read())
-        elif path == '/about':
-            with open('about.html', 'rb') as page:
-                self.wfile.write(page.read())
+        if self.path == '/':
+            file_path = pathlib.Path('home.html')
+            if file_path.is_file():
+                self.send_response(HTTPStatus.OK)
+                self.send_header('Content-Type', 'text/html')
+                self.end_headers()
+                with file_path.open('rb') as f:
+                    self.wfile.write(f.read())
+            else:
+                self.send_response(HTTPStatus.NOT_FOUND)
+                self.end_headers()
+        else:
+            self.send_response(HTTPStatus.OK)
+            self.end_headers()
+            msg = 'Hello! you requested %s' % (self.path)
+            self.wfile.write(msg.encode())
 
 
 port = int(os.getenv('PORT', 80))
