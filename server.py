9013ce659e6892977500f8e5900d59c696fa3873
David Yates <David@recursiveav.co.uk>
2023-01-04 16:31:05 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-app = Flask(__name__)
+class Server(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # get the page
+        path = self.path
+        if path == '/':
+            with open('home.html', 'rb') as page:
+                self.wfile.write(page.read())
+        elif path == '/about':
+            with open('about.html', 'rb') as page:
+                self.wfile.write(page.read())
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+try:
+    server = HTTPServer(('localhost', 8080), Server)
+    server.serve_forever()
+except:
+    pass
+
+server.server_close()
