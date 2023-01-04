5bf29628604c62a44f13e881456b395c3d6d4670
David Yates <David@recursiveav.co.uk>
2023-01-04 17:18:23 +0000

    revised Text from ChatGPT suggestions

--- server.py
+++ server.py
@@ -1,16 +1,29 @@
 import os
 import http.server
 import socketserver
+import pathlib
 
 from http import HTTPStatus
 
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(HTTPStatus.OK)
-        self.end_headers()
-        msg = 'Hello! you requested %s' % (self.path)
-        self.wfile.write(msg.encode())
+        if self.path == '/':
+            file_path = pathlib.Path('home.html')
+            if file_path.is_file():
+                self.send_response(HTTPStatus.OK)
+                self.send_header('Content-Type', 'text/html')
+                self.end_headers()
+                with file_path.open('rb') as f:
+                    self.wfile.write(f.read())
+            else:
+                self.send_response(HTTPStatus.NOT_FOUND)
+                self.end_headers()
+        else:
+            self.send_response(HTTPStatus.OK)
+            self.end_headers()
+            msg = 'Hello! you requested %s' % (self.path)
+            self.wfile.write(msg.encode())
 
 
 port = int(os.getenv('PORT', 80))
