1e6e3c4a52c2417ecc4f04c50245c8021717034d
David Yates <David@recursiveav.co.uk>
2023-01-04 17:41:25 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,41 +1,19 @@
 import os
-import http.server
-import socketserver
-import pathlib
-import websocket
+import asyncio
+import websockets
 
-from http import HTTPStatus
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print(f"< {name}")
 
+    greeting = f"Hello {name}!"
 
-class Handler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == '/':
-            file_path = pathlib.Path('home.html')
-            if file_path.is_file():
-                self.send_response(HTTPStatus.OK)
-                self.send_header('Content-Type', 'text/html')
-                self.end_headers()
-                with file_path.open('rb') as f:
-                    self.wfile.write(f.read())
-            else:
-                self.send_response(HTTPStatus.NOT_FOUND)
-                self.end_headers()
-        else:
-            self.send_response(HTTPStatus.OK)
-            self.end_headers()
-            msg = 'Hello! you requested %s' % (self.path)
-            self.wfile.write(msg.encode())
-
-        # Send a WebSocket message to any connected clients
-        if 'websocket' in self.headers:
-            ws = websocket.WebSocket()
-            ws.connect(self.headers['websocket'], self.headers)
-            ws.send('Hello from the server!')
-            ws.close()
-
+    await websocket.send(greeting)
+    print(f"> {greeting}")
 
 port = int(os.getenv('PORT', 80))
 print('Listening on port %s' % (port))
-httpd = socketserver.TCPServer(('', port), Handler)
-httpd.serve_forever()
+start_server = websockets.serve(hello, '', port)
 
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
