f015ca3fea10225e213e33ef98706d4908257c3b
David Yates <David@recursiveav.co.uk>
2023-01-04 17:45:24 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,33 @@
 import os
-import asyncio
-import websockets
+import http.server
+import socketserver
+import pathlib
 
-async def hello(websocket, path):
-    name = await websocket.recv()
-    print(f"< {name}")
+from http import HTTPStatus
 
-    greeting = f"Hello {name}!"
 
-    await websocket.send(greeting)
-    print(f"> {greeting}")
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            file_path = pathlib.Path('home.html')
+            if file_path.is_file():
+                self.send_response(HTTPStatus.OK)
+                self.send_header('Content-Type', 'text/html')
+                self.end_headers()
+                with file_path.open('rb') as f:
+                    self.wfile.write(f.read())
+            else:
+                self.send_response(HTTPStatus.NOT_FOUND)
+                self.end_headers()
+        else:
+            self.send_response(HTTPStatus.OK)
+            self.end_headers()
+            msg = 'Hello! you requested %s' % (self.path)
+            self.wfile.write(msg.encode())
+
 
 port = int(os.getenv('PORT', 80))
 print('Listening on port %s' % (port))
-start_server = websockets.serve(hello, '', port)
+httpd = socketserver.TCPServer(('', port), Handler)
+httpd.serve_forever()
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
