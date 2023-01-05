c7f742b8484bd4c5ffb2d62eec7f8838d1de42cf
David Yates <David@recursiveav.co.uk>
2023-01-05 15:40:27 +0000

    Update server.py
    Added additional code to send a message every 10 seconds to the web socket client

--- server.py
+++ server.py
@@ -7,34 +7,34 @@ import websockets
 
 from http import HTTPStatus
 
-
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/':
-            file_path = pathlib.Path('home.html')
-            if file_path.is_file():
-                self.send_response(HTTPStatus.OK)
-                self.send_header('Content-Type', 'text/html')
-                self.end_headers()
-                with file_path.open('rb') as f:
-                    self.wfile.write(f.read())
-            else:
-                self.send_response(HTTPStatus.NOT_FOUND)
-                self.end_headers()
-        else:
+    if self.path == '/':
+        file_path = pathlib.Path('home.html')
+        if file_path.is_file():
             self.send_response(HTTPStatus.OK)
+            self.send_header('Content-Type', 'text/html')
             self.end_headers()
-            msg = 'Hello! you requested %s' % (self.path)
-            self.wfile.write(msg.encode())
-            
-            
-            # Send a WebSocket message to any connected clients
-        if 'websocket' in self.headers:
-            ws = websocket.WebSocket()
-            ws.connect(self.headers['websocket'], self.headers)
-            ws.send('Hello from the server!')
-            ws.close()
+            with file_path.open('rb') as f:
+                self.wfile.write(f.read())
+        else:
+            self.send_response(HTTPStatus.NOT_FOUND)
+            self.end_headers()
+    else:
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        msg = 'Hello! you requested %s' % (self.path)
+        self.wfile.write(msg.encode())
 
+    # Check if the request includes a "websocket" header
+    if 'websocket' in self.headers:
+        # Establish a WebSocket connection with the client
+        ws = websocket.WebSocket()
+        ws.connect(self.headers['websocket'], self.headers)
+
+        # Send a message to the client
+        ws.send('Hello from the server!')
+        ws.close()
 
 port = int(os.getenv('PORT', 80))
 print('Listening on port %s' % (port))
